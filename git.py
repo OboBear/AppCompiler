b17e09b77fef27e425f50d00102ec640251f1ef0
uuu1993 <634468460@qq.com>
2016-01-04 16:32:15 +0800

    update git.py

--- git.py
+++ git.py
@@ -1,13 +1,17 @@
 import web
 import commands
+import os
+
 urls=(
     '/pull','git',
 )
 
 class git:
     def GET(self):
-        (status,output) = commands.getstatusoutput('bash pull.sh')
-        return "success"
+        # (status,output) = commands.getstatusoutput('bash pull.sh')
+        # return "success"
+        os.system('pull.sh')
+        return 'success'
 
 if __name__ == "__main__":
     app = web.application(urls,globals())
